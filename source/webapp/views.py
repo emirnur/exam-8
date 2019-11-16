8b5fcb7be8f2808b78deba21251b459948cf4ae4
emirnur <nur.emirlan@gmail.com>
2019-11-16 14:37:27 +0600

    add CRUD for Product

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from webapp.forms import ProductForm
+from webapp.models import Product
+
+
+class IndexView(ListView):
+    model = Product
+    template_name = 'index.html'
+
+
+class ProductView(DetailView):
+    model = Product
+    template_name = 'product/detail.html'
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'product/create.html'
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    template_name = 'product/update.html'
+    context_object_name = 'product'
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('webapp:product_detail', kwargs={'pk': self.object.pk})
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'product/delete.html'
+    success_url = reverse_lazy('webapp:index')
+    context_object_name = 'product'
+
